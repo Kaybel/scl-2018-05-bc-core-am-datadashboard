b2cc2fdd45ecc1fe1ca3b22bf1c9221fb3244b84
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-20 09:35:03 -0400

    cambios en botones

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const btncohort = document.getElementById('btn-cohort');
+const btnlecturas = document.getElementById('btn-lecturas');
+const btnquizzes = document.getElementById('btn-quizzes');
+const btncompletitud = document.getElementById('btn-completitud');
+
+btncohort.addEventListener('click', () => {
+  /* Aquí se debería mostrar a las alumnas */
+});
