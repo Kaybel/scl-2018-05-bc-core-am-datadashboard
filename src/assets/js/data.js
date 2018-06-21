2a747cc7d929f35e27ba620d4b5675814a2afd56
Kaybel <kblmrtnz@gmail.com>
2018-06-21 14:58:41 -0400

    se intenta aplicar for hay dos errores en data js

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,7 +1,19 @@
 const btnAlumnas = document.getElementById('btn-alumnas');
-/*const btnStudents = document.getElementById('btn-alumnas'); */
+const btnProgreso = document.getElementById('botonesDeDatosProgreso');
 const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const progressJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+const cohortsJSON = '../../../../data/cohorts/cohorts.json';
+let cohorts = Object.entries(cohortsJSON);
+let data = Object.entries(usersJSON);
+let progress = Object.entries(progressJSON).find(element => element[0] == progress.id)[1];
+let resultPercent = '';
 
+let coh = (cohort, (elementJSON)  => {
+
+/*  forEach( in cohortsJSON); {
+    return cohorts[0]};
+});
+*/  
 fetch(usersJSON)
   .then(response => response.json())
   .then(data => {
@@ -9,6 +21,19 @@ fetch(usersJSON)
     renderUsers(data);
   });
 
+fetch(progressJSON)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    renderUsers(data);
+  });
+
+fetch(cohortsJSON)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    renderUsers(data);
+  });
 
 const renderUsers = data => {
   btnAlumnas.addEventListener('click', () => {
@@ -23,13 +48,14 @@ const renderUsers = data => {
 
 
 window.computeUsersStats = (user, progress, courses) => {
-  
+
 };
+
 window.sortUsers = (users, orderBy, orderDirection) => {
 
 };
 window.filterUsers = (users, search) => {
-
+  
 };
 window.processCohortData = (options) => {
 
