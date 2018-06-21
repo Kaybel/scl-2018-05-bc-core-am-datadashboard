796ad71f76b1d509af57fb98c311f3c3cc487abb
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-21 15:02:48 -0400

    creación base de objetos principales exercises, reads y quizzes

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,25 +1,40 @@
-const btnAlumnas = document.getElementById('btn-alumnas');
-/*const btnStudents = document.getElementById('btn-alumnas'); */
 const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+const cohortsJSON = '../../../data/cohorts.json';
+const progressJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
 
 fetch(usersJSON)
   .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    renderUsers(data);
-  });
+  .then(users => {
+    console.log(users);
+  })
+
+fetch(cohortsJSON)
+  .then(response => response.json())
+  .then(cohorts => {
+    console.log(cohorts);
+  })
+
+fetch(progressJSON)
+  .then(response => response.json())
+  .then(progress => {
+    console.log(progress);
+  })
+}
 
 
-const renderUsers = data => {
-  btnAlumnas.addEventListener('click', () => {
-    // forIn recorre objetos y accede a las propiedades
-    // for y forEach recorre arrays
-    const render = data.forEach(element => {
-      return containerStudents.innerHTML += `<p>${element.name.toUpperCase()}</p>`;
-    });
-    return render;
-  });
-}; 
+class Reads {
+  constructor (total, completed, percent) {
+    this.total = total,
+    this.completed = gender,
+    this.percent = percent,
+    
+  }
+}
+
+/* */
+
+let courses = Object.entries(progress).find(element => element[0] == users.id)[1];
+
 
 
 window.computeUsersStats = (user, progress, courses) => {
