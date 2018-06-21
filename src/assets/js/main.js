af97cc53f5e04d1b57b734be3c5a9c6761ef9a5b
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-21 08:20:50 -0400

    prueba

---
+++ src/assets/js/main.js
@@ -0,0 +1,15 @@
+const btnCohort = document.getElementById('btn-cohort');
+const botonalumnas = document.getElementById('btn-alumnas');
+
+
+btnCohort.addEventListener('click', () => {
+  document.getElementById('navbar').style.display = 'block';
+  document.getElementById('divbtn-alumnas').style.display = 'block'; 
+  document.getElementById('btnCohort').style.display = 'none';
+  document.getElementById('imgLaboratoria').style.display = 'none';
+});
+
+botonalumnas.addEventListener('click', () => {
+  document.getElementById('botonesDeDatos').style.display = 'block';
+  
+});
--- src/main.js
+++
@@ -1,8 +0,0 @@
-const btncohort = document.getElementById('btn-cohort');
-const btnlecturas = document.getElementById('btn-lecturas');
-const btnquizzes = document.getElementById('btn-quizzes');
-const btncompletitud = document.getElementById('btn-completitud');
-
-btncohort.addEventListener('click', () => {
-  /* Aquí se debería mostrar a las alumnas */
-});
