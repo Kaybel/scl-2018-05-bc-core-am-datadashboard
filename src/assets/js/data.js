05d7423446876bf6b3ec924edc739fcb9075eeee
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-22 09:17:42 -0400

    Declaración de todos los objetos constructores, más comentarios para mejorar entendimiento de lo que se debe hacer

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,4 +1,4 @@
-/*const btnAlumnas = document.getElementById('btn-alumnas');
+/* const btnAlumnas = document.getElementById('btn-alumnas');
 const btnProgreso = document.getElementById('botonesDeDatosProgreso');*/
 
 const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
@@ -6,9 +6,15 @@ const progressJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.jso
 const cohortsJSON = '../../../../data/cohorts/cohorts.json';
 
 /* Variables que guardarán json */
-let users = {};
-let progress = {};
-let cohorts = {};
+let users = []; // Arreglo de usuarios de la data EN BRUTO
+
+let progress = {}; // Objeto de progreso EN BRUTO, contiene un id para cada usuario con un objeto que contiene el progreso del usuario por cada curso
+
+let cohorts = {}; // Arreglo de cohorts con la data EN BRUTO
+
+/* para armar courses, se debe acceder al objeto progress */
+let courses = []; // Arreglo de strings con los ids de los cursos del cohort en cuestión. Esta data se puede extraer de la propiedad coursesIndex de los objetos que representan los cohorts.
+
 
 /* Promesas (retorno de json) */
 fetch(usersJSON)
@@ -35,24 +41,60 @@ fetch(cohortsJSON)
     renderUsers(cohorts);
   });
 
+
 /* Declaración de objetos (constructor) */
+class Usuario {
+  constructor(Stats) {
+    this.stats = stats
+  }
+}
+
+class Stats {
+  constructor(Percent, Exercises, Reads, Quizzes) {
+    this.Percent = Percent,
+    this.Exercises = Exercises,
+    this.Reads = Reads,
+    this.Quizzes = Quizzes
+  }
+}
+
 
 /* Construcción objeto Quizzes */
 class Quizzes {
-  constructor (total, completed, percent, scoreSum, scoreAvg) {
-    this.total = total;
-    this.completed = completed;
-    this.percent = percent;
-    this.scoreSum = scoreSum;
-    this.scoreAvg = scoreAvg;
+  constructor(total, completed, percent, scoreSum, scoreAvg) {
+    this.total = total,
+    this.completed = completed,
+    this.percent = percent,
+    this.scoreSum = scoreSum,
+    this.scoreAvg = scoreAvg
   }
 }
 
 /* Construcción objeto Reads */
+class Reads {
+  constructor(total, completed, percent) {
+    this.total = total,
+    this.completed = completed,
+    this.percent = percent
+  }
+}
 
 /* Construcción objeto Exercises */
+class Exercises {
+  constructor(total, completed, percent) {
+    this.total = total,
+    this.completed = completed,
+    this.percent = percent
+  }
+}
 
-/* Construcción objeto Progress */
+/* Construcción Progress */
+/* % de progreso gral de cada estudiante, de todos sus cursos. Se encuentra en objeto progress. Detalle: Hay usuarios que no tienen nada, está vacío */
+let progressStudent = 
+
+/* if (para hacer match por el id */
+
+/* for in anidados*/
 
 
 window.computeUsersStats = (user, progress, courses) => {
