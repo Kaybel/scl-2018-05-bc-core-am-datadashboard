d5d0f0bf342d82125d38a75dbc4c735c95279631
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-24 17:50:57 -0400

    Agregado suma de lecturas, de quizzes, ejercicios, promedio quizzes y suma de puntajes

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -30,6 +30,13 @@ fetch(progressJSON)
     progress = data;
     console.log(progress);
   });
+
+fetch(cohortsJSON)
+  .then(response => response.json())
+  .then(data => {
+    cohorts = data;
+    console.log(cohorts);
+  });
   
 
 /* Declaración de objetos globales (constructor) */
@@ -88,59 +95,53 @@ class Percent {
 }
 
 
-/* if (para hacer match por el id */
-
-/*progressArray = Object.entries(progress);
-progressArray = [];
-introArray = [];
-percentArray = [];
-for (i = 0; i < progressArray.length ; i++) {
-introArray.push(Object.entries(progressArray[i][1]));
-console.log(introArray[i]["0"][1].percent);
-} */
-
-for(let idEstudiante in progress){console.log("ID > " + idEstudiante +JSON.stringify(progress[idEstudiante]))}
-
-
-
- let lect = 0
-for(let idEstudiante in progress) {
- 
-  //console.log("ID > "+JSON.stringify(progress[idEstudiante]));
-	  for(let idCurso in progress[idEstudiante]) {
-      let completedlect = 0;
-      //console.log("ID CURSO > "+JSON.stringify(progress[idEstudiante][idCurso]));
-        for (let idUnits in progress[idEstudiante][idCurso]) {
-          
-          //console.log("ID estudiante > " + idEstudiante + JSON.stringify((progress[idEstudiante][idCurso])[idUnits]));
-            for (let indexParts in progress[idEstudiante][idCurso][idUnits]) {
-              //console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts]));
-              
-              for (let indexCourses in progress[idEstudiante][idCurso][idUnits][indexParts]) {
-                //console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]));
-                
-                for (let indextype in progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]) {
-                  let chapter = ((progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype])); 
-                  console.log(chapter);
-                  
-                  if (chapter.type === "read"){
-                    lect++;
-            
-                  }
-                  if (chapter.type === "read" && chapter.completed === 1){
-                    completedlect = completedlect + 1;
-              
-                  }                  
-                }               
-              }           
+let lect = 0;
+let quiz = 0;
+for (let idEstudiante in progress) {
+	  for (let idCurso in progress[idEstudiante]) {
+    let completedlect = 0;
+    let completedquiz = 0;
+    let lect = 0;
+    let quiz = 0;
+    let exercises = 0;
+    let completedexercises = 0;
+    let scoreSum = 0;
+    let scoreAvg = 0;
+    for (let idUnits in progress[idEstudiante][idCurso]) {
+      for (let indexParts in progress[idEstudiante][idCurso][idUnits]) {
+        for (let indexCourses in progress[idEstudiante][idCurso][idUnits][indexParts]) {
+          for (let indextype in progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]) {
+            let chapter = ((progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype])); 
+            if (chapter.type === 'read') {
+              lect++;
             }
+            if (chapter.type === 'read' && chapter.completed === 1) {
+              completedlect++;
+            }                 
+            if (chapter.type === 'quiz') {
+              quiz++;
+            } 
+            if (chapter.type === 'quiz' && chapter.completed === 1) {
+              completedquiz++;
+              scoreSum += chapter.score;
+            }
+            if (chapter.type === 'practice') {
+              exercises++;
+            }
+            if (chapter.type === 'practice' && chapter.completed === 1) {
+              completedexercises++;
+            }
+          }               
         }
-        console.log('para la estudiante de ID ' + idEstudiante + ' las lecturas realizadas son: ' + completedlect);    
-    }     
+      }
+    }
+    let percentLect = Math.round((completedlect / lect) * 100); 
+    let percentQuiz = Math.round((completedquiz / quiz) * 100);
+    let percentExercises = Math.round((completedexercises / exercises) * 100);
+    scoreAvg = scoreSum / completedquiz;
+    console.log(idEstudiante + ' > ' + 'Lecturas totales: ' + lect + ' Lecturas completadas: ' + completedlect + ' % total realizado en lecturas: ' + percentLect + ' Quizzes totales: ' + quiz + ' Quizzes completados: ' + completedquiz + ' Suma de puntajes quizzes: ' + scoreSum + ' Promedio Quizzes: ' + scoreAvg + ' Ejercicios totales: ' + exercises + ' Ejercicios completados: ' + completedexercises);
+  }
 }
-
-               
-
 
 
 window.computeUsersStats = (user, progress, courses) => {
