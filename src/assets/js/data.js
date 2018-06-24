435121dc514fb1a8491688684288d61519e8583a
Kaybel <kblmrtnz@gmail.com>
2018-06-23 22:36:30 -0400

    esta es la idea que aun no funciona, se intenta anidar dentro de una funcion para poder hacer variables que retengan la info

--- src/assets/js/data.js
+++ src/assets/js/data.js
@@ -1,9 +1,9 @@
-/* const btnAlumnas = document.getElementById('btn-alumnas');
-const btnProgreso = document.getElementById('botonesDeDatosProgreso');*/
-
+const btnAlumnas = document.getElementById('btn-alumnas');
+const btnLectura = document.getElementById('btn-lecturas');
+const btnQuizzes = document.getElementById('btn-quizzes');
+const btnCompletitud = document.getElementById('btn-completitud');
 const usersJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
 const progressJSON = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
-const cohortsJSON = '../../../data/cohorts.json';
 
 /* Variables que guardarán json */
 let users = []; // Arreglo de usuarios de la data EN BRUTO
@@ -87,7 +87,7 @@ class Percent {
 }
 /* if (para hacer match por el id */
 
-/*progressArray = Object.entries(progress);
+/* progressArray = Object.entries(progress);
 progressArray = [];
 introArray = [];
 percentArray = [];
@@ -96,23 +96,26 @@ introArray.push(Object.entries(progressArray[i][1]));
 console.log(introArray[i]["0"][1].percent);
 } */
 
-for (let idEstudiante in progress) { console.log("ID > " + idEstudiante + JSON.stringify(progress[idEstudiante])) }
+/*    CON  ESTO INGRESAMOS A LAS CARPETAS PERO NO ALMACENAMOS EL VALOR ASI QUE EN TEORIA NO PODRIAMOS MANEJAR LA INFO
+
+
+for (let idEstudiante in progress) {console.log('ID > ' + idEstudiante + JSON.stringify(progress[idEstudiante]))};
 
 
 for (let idEstudiante in progress) {
-  //console.log("ID > "+JSON.stringify(progress[idEstudiante]));
+  // console.log("ID > "+JSON.stringify(progress[idEstudiante]));
   for (let idCurso in progress[idEstudiante]) {
-    //console.log("ID CURSO > "+JSON.stringify(progress[idEstudiante][idCurso]));
+    // console.log("ID CURSO > "+JSON.stringify(progress[idEstudiante][idCurso]));
     for (let idUnits in progress[idEstudiante][idCurso]) {
-      //console.log("ID estudiante > " + idEstudiante + JSON.stringify((progress[idEstudiante][idCurso])[idUnits]));
+      // console.log("ID estudiante > " + idEstudiante + JSON.stringify((progress[idEstudiante][idCurso])[idUnits]));
       for (let indexParts in progress[idEstudiante][idCurso][idUnits]) {
-        //console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts]);
+        // console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts]);
         for (let indexCourses in progress[idEstudiante][idCurso][idUnits][indexParts]) {
-          //console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]));
+          // console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]));
           for (let indextype in progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses]) {
-            //console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype])); 
+            // console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype])); 
             for (let datosFinales in progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype]) {
-              console.log("ID estudiante > " + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype][datosFinales]));
+              console.log('ID estudiante > ' + idEstudiante + JSON.stringify(progress[idEstudiante][idCurso][idUnits][indexParts][indexCourses][indextype][datosFinales]));
             }
           }
         }
@@ -121,15 +124,43 @@ for (let idEstudiante in progress) {
   }
 }
 
+*/ 
+
+
+// Esta es la idea que tengo pero no importa como lo plantee sigue sin imprimir la info 
+// cabe destacar que ahora no imprime nada...
+
+
+const ingresarACarpetasUser = (usersJSON => {
+  const usersJSON1 = '../../../data/cohorts/lim-2018-03-pre-core-pw/users.json';
+  // primero intentaba hacer que imprimiera otra vez la lista de nombres.
+  btnAlumnas.addEventListener('click', () =>{
+    const render = usersJSON1.forEach(element => {
+      return container.innerHTML += `<p>${element.name.toUpperCase()}</p>`
+    });
+  });
+  // se crea otra variable para dentro de una funcion comenzar a crear la ruta con el for y con los if porque las variables del for no guardan la info fuera del mismo for.
+});
+
+
+const ingresarACarpetasProgress = (progressJSON => {
+  const progressJSON1 = '../../../data/cohorts/lim-2018-03-pre-core-pw/progress.json';
+});
+
+const ingresarACohorts = (cohortsJSON=>{
+  const cohortsJSON1 = '../../../data/cohorts.json';
+});
+
 /* Aquí dejo planteado procesamiento para lecturas completadas y total de lecturas */
+/*
 let totalReads = 0;
 let completedReads = 0;
-if ('type' == 'read' && 'completed' == 1) {
+if ('type' === 'read' && 'completed' === 1) {
   completedReads = CompletedReads++;
-} else if ('type' == 'read') {
+} else if ('type' === 'read') {
   totalReads++;
 }
-
+*/
 
 window.computeUsersStats = (user, progress, courses) => {
 
